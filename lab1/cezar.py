512863f1e967ddb369cc0b1f9b530df7b26b5685
Pięćdziesiątka Trygonometryczna <dawidkalinowski243@gmail.com>
2024-03-14 23:14:27 +0100

    siema

--- lab1/cezar.py
+++ lab1/cezar.py
@@ -1,13 +1,17 @@
 import sys
+import math
+# alphabet = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'r', 's', 'ś', 't', 'u', 'w', 'y', 'z', 'ź', 'ż']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+crypto = open("crypto.txt", "r").read()
+plain = open("plain.txt", "r").read().lower()
+
+
 def cezarCrypt():
-    plain = open("plain.txt", "r").read().lower()
-    # alphabet = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'r', 's', 'ś', 't', 'u', 'w', 'y', 'z', 'ź', 'ż']
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     try:
-        tryKey = int(open("key.txt", "r").read().split()[0]) 
+        tryKey = int(open("key.txt", "r").read().split()[0])
         if tryKey < 0:
-            raise ValueError("Nieprawidłowy klucz.")
-    except (ValueError, IndexError):
+            raise ValueError("Klucz nie może być mniejszy od 0.")
+    except (IndexError):
         raise ValueError("Nieprawidłowy klucz.")
     key = tryKey % len(alphabet)
 
@@ -25,11 +29,37 @@ def cezarCrypt():
                 break
     with open("crypto.txt", "w") as cryptoTxt:
         cryptoTxt.write(crypted)
+    print(f"Tekst został pomyślnie zaszyfrowany szyfrem Cezara o kluczu {key} w pliku crypto.txt")
+
+
+def affineCrypt():
+    try:
+        tryKey = int(open("key.txt", "r").read().split()[0]) 
+        factor = int(open("key.txt", "r").read().split()[1])
+        
+        if tryKey < 0 or factor < 0:
+            raise ValueError("Klucz i współczynnik nie mogą być mniejsza od 0.")
+        if math.gcd(factor, len(alphabet)) != 1:
+            raise ValueError("Współczynnik nie jest względnie pierwszy z długością alfabetu.")
+    except (IndexError):
+        raise ValueError("Nieprawidłowy klucz.")
+    
+    key = tryKey % len(alphabet)
+
+    crypted = ""
+    for i in range(len(plain)):
+        for j in range(len(alphabet)):
+            if plain[i] == " ":
+                crypted += " "
+                break
+            elif plain[i] == alphabet[j]:
+                crypted += alphabet[(factor * j + key) % len(alphabet)]
+                break
+    with open("crypto.txt", "w") as cryptoTxt:
+        cryptoTxt.write(crypted)
+    print(f"Tekst został pomyślnie zaszyfrowany szyfrem alfinicznym o kluczu {key} oraz współczynniku {factor} w pliku crypto.txt")
 
 def cezarDecrypt():
-    crypto = open("crypto.txt", "r").read()
-    # alphabet = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'r', 's', 'ś', 't', 'u', 'w', 'y', 'z', 'ź', 'ż']
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     try:
         tryKey = int(open("key.txt", "r").read().split()[0]) 
         if tryKey < 0:
@@ -52,14 +82,43 @@ def cezarDecrypt():
                 break
     with open("decrypt.txt", "w") as decryptTxt:
         decryptTxt.write(decrypted)
+    print(f"Tekst został pomyślnie rozszyfrowany. Tekst jawny został zapisany w pliku decrypt.txt")
+
+def affineDecrypt():
+    try:
+        tryKey = int(open("key.txt", "r").read().split()[0]) 
+        factor = int(open("key.txt", "r").read().split()[1])
+        
+        if tryKey < 0 or factor < 0:
+            raise ValueError("Nieprawidłowy klucz.")
+        
+        if math.gcd(factor, len(alphabet)) != 1:
+            raise ValueError("Współczynnik nie jest względnie pierwszy z długością alfabetu.")
+    except (ValueError, IndexError):
+        raise ValueError("Nieprawidłowy klucz.")
+    
+    key = tryKey % len(alphabet)
+
+    decrypted = ""
+    for i in range(len(crypto)):
+        for j in range(len(alphabet)):
+            if crypto[i] == " ":
+                decrypted += " "
+                break
+            elif crypto[i] == alphabet[j]:
+                factor_inv = pow(factor, -1, len(alphabet))
+                decrypted += alphabet[(factor_inv * (j - key)) % len(alphabet)]
+                break
+    with open("decrypt.txt", "w") as decryptTxt:
+        decryptTxt.write(decrypted)
+    print(f"Tekst został pomyślnie rozszyfrowany. Tekst jawny został zapisany w pliku decrypt.txt")
+
+
 
 
 def cezarKryptoanaliza():
     with open("decrypt.txt", "w") as decryptTxt:
         decryptTxt.write("")
-    crypto = open("crypto.txt", "r").read()
-    # alphabet = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'r', 's', 'ś', 't', 'u', 'w', 'y', 'z', 'ź', 'ż']
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     for key in range(1, len(alphabet)):    
         decrypted = ""
         for i in range(len(crypto)):
@@ -75,7 +134,87 @@ def cezarKryptoanaliza():
                     break
         with open("decrypt.txt", "a") as decryptTxt:
             decryptTxt.write(decrypted + "\n")
+    print(f"Kryptoanaliza szyfru Cezara zakończona pomyślnie. Rozszyfrowane teksty znajdują się w pliku decrypt.txt")
 
+
+def affineKryptoanaliza():
+    with open("decrypt.txt", "w") as decryptTxt:
+        decryptTxt.write("")
+        
+    for key in range(1, len(alphabet)):
+        for factor in range(1, len(alphabet)):
+            if math.gcd(factor, len(alphabet)) == 1:
+                decrypted = ""
+                for i in range(len(crypto)):
+                    for j in range(len(alphabet)):
+                        if crypto[i] == " ":
+                            decrypted += " "
+                            break
+                        elif crypto[i] == alphabet[j]:
+                            factor_inv = pow(factor, -1, len(alphabet))
+                            decrypted += alphabet[(factor_inv * (j - key)) % len(alphabet)]
+                            break
+                with open("decrypt.txt", "a") as decryptTxt:
+                    decryptTxt.write(decrypted + "\n")
+    print(f"Kryptoanaliza szyfru alfinicznego zakończona pomyślnie. Rozszyfrowane teksty znajdują się w pliku decrypt.txt")
+
+def cezarKryptoanalizaJawna():
+    success = False
+    extra = open("extra.txt", "r").read()
+    for key in range(1, len(alphabet)):    
+        decrypted = ""
+        for i in range(len(crypto)):
+            for j in range(len(alphabet)):
+                if crypto[i] == " ":
+                    decrypted += " "
+                    break
+                elif crypto[i] == alphabet[j]:
+                    if j - key < 0:
+                        decrypted += alphabet[len(alphabet) - key + j]
+                    else:
+                        decrypted += alphabet[j - key]
+                    break
+        if extra in decrypted:
+            success = True
+            print(f"Udało się odnaleźć klucz - {key}! Odszyfrowany tekst został zapisany w pliku decrypt.txt")
+            with open("decrypt.txt", "w") as decryptTxt:
+                decryptTxt.write(decrypted)
+            with open("key-new.txt", "w") as keyTxt:
+                keyTxt.write(str(key))
+            break
+    if success == False:
+        raise ValueError("Nie udało się odnaleźć klucza.")
+    
+def affineKryptoanalizaJawna():
+    success = False
+    extra = open("extra.txt", "r").read()
+    for key in range(1, len(alphabet)):
+        for factor in range(1, len(alphabet)):
+            if math.gcd(factor, len(alphabet)) == 1:
+                decrypted = ""
+                for i in range(len(crypto)):
+                    for j in range(len(alphabet)):
+                        if crypto[i] == " ":
+                            decrypted += " "
+                            break
+                        elif crypto[i] == alphabet[j]:
+                            factor_inv = pow(factor, -1, len(alphabet))
+                            decrypted += alphabet[(factor_inv * (j - key)) % len(alphabet)]
+                            break
+                # print(decrypted)
+        if extra in decrypted:
+            success = True
+            print(f"Udało się odnaleźć klucz - {key} - oraz współczynnik - {factor}! Odszyfrowany tekst został zapisany w pliku decrypt.txt.\n"
+                  "Klucz oraz współczynnik zostały zapisane w pliku key-new.txt")
+            with open("decrypt.txt", "w") as decryptTxt:
+                decryptTxt.write(decrypted)
+            with open("key-new.txt", "w") as keyTxt:
+                keyTxt.write(str(key), " ", str(factor))
+            break
+    # if success == False:
+    #     raise ValueError("Nie udało się odnaleźć klucza.")
+
+        
 
 
 if __name__ == "__main__":
@@ -88,7 +227,7 @@ if __name__ == "__main__":
         elif arg2 == "-d":
             cezarDecrypt()
         elif arg2 == "-j":
-            print("na razie nie ma -j")
+            cezarKryptoanalizaJawna()
         elif arg2 == "-k":
             cezarKryptoanaliza()
         else:
@@ -96,13 +235,13 @@ if __name__ == "__main__":
         
     elif arg1 == "-a":
         if arg2 == "-e":
-            print()
+            affineCrypt()
         elif arg2 == "-d":
-            print()
+            affineDecrypt()
         elif arg2 == "-j":
-            print()
+            affineKryptoanalizaJawna()
         elif arg2 == "-k":
-            print()
+            affineKryptoanaliza()
         else:
             raise Exception("Nieprawidłowy drugi argument")
     else:
